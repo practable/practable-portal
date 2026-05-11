301a727730e5cefeca1745fc5754c5039fa671cc
Imogen Wren <imogen.wren91@gmail.com>
2026-05-11 10:41:56 +0100

    Update install_portal.sh

--- install_portal.sh
+++ install_portal.sh
@@ -3,9 +3,11 @@ set -e    # make sure that process exits if anything fails
 
 # Scrip to install and setup everything required for the operation of practable portal, above and beyond the install required for typical practable experiments
 
+
+
 #create the python venv called .portalenv
 echo "Creating Python .portalvenv"
-python3 -m venv .portalenv
+python3 -m venv .portalvenv
 
 echo "Venv Created, Entering .portalvenv"
 source .portalvenv/bin/activate
